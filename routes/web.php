83eb2bd4f67bd7dc944a49a1f43a1f85361ab645
Yoshikawa Taiki <yoshikawataiki@gmail.com>
2023-03-09 22:59:44 +0900

    add PracticeController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('practice', function() {
-    return response('practice');
-});
+// Route::get('practice', function() {
+//     return response('practice');
+// });
 
-Route::get('practice2', function() {
-    $test = 'practice2';
-    return response($test);
-});
+// Route::get('practice2', function() {
+//     $test = 'practice2';
+//     return response($test);
+// });
 
-Route::get('practice3', function() {
-    $test = 'test';
-    return response($test);
-});
+// Route::get('practice3', function() {
+//     $test = 'test';
+//     return response($test);
+// });
+
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);

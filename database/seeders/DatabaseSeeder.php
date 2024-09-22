a50e314955b68678c93dff10ae518bcf52b0e5a8
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2024-09-23 01:08:10 +0900

    DatabaseSeederにSheetsがあるとエラーになるため、コメントアウト

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            SheetTableSeeder::class,
-        ]);
+        // $this->call([
+        //     SheetTableSeeder::class,
+        // ]);
     }
 }

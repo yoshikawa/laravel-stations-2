012fe51c0c0664d4bf09c052e8fc37a77a2226e3
よしかわたいき <yoshikawataiki@gmail.com>
2024-10-07 01:24:33 +0900

    Update database/seeders/DatabaseSeeder.php
    Co-authored-by: choclucy <56016475+choclucy@users.noreply.github.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call([
-        //     SheetTableSeeder::class,
-        // ]);
+        $this->call([
+            // ここに Seeder を追加する
+        ]);
     }
 }
